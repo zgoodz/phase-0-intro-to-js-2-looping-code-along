c9bf54f0fff6918e5226573ef0d7fe6b84e39a44
Zachary Goodman <znote89@gmail.com>
2021-06-07 12:00:28 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return messages;
+}
+
+function countDown() {
+    let number = 10;
+    while (number >= 0) {
+        console.log(number--)
+    }
+}
